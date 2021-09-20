635a6586722319b294710fcef64fe336539411bc
nicolas <nmercier@d-edge.com>
2021-09-20 14:46:23 +0200

    chore: re enabling error middleware

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-const errorMiddleware = (error: any, request: Request, response: Response) => {
-  // const status = error.status || 500;
-  // const message = error.message || 'Something went wrong';
-  // response.status(status).send({
-  //   message,
-  //   status,
-  // });
+const errorMiddleware = (error: any, request: Request, response: Response, next: NextFunction) => {
+  const status: number = error.status || 500;
+  const message: string = error.message || 'Something went wrong';
+  response.status(status).send({
+    message,
+    status,
+  });
 };
 
 export default errorMiddleware;
